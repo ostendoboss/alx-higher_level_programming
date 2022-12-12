fa2896bdbf3c928b62a36caa84796770201041a3
oyeyemialawode <oyeyemialawode@gmail.com>
2022-12-12 06:29:31 +0100

    pi

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
-i#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+#!/usr/bin/python3
+str = "#Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 str = str[39:67] + str[-22:-17] + str[0:6]
 print(str)
-
