49b7777f53f53e0daf85b468f14a14ac4bd8b782
oyeyemialawode <oyeyemialawode@gmail.com>
2023-01-02 14:09:32 +0100

    tasks

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,19 +7,20 @@
 */
 
 void reverse_listint(listint_t **head)
+{
+        listint_t *prev = NULL;
+        listint_t *current = *head;
+        listint_t *next = NULL;
 
-{
-listint_t *prev = NULL;
-listint_t *current = *head;
-listint_t *next = NULL;
-while (current)
-{
-next = current->next;
-current->next = prev;
-prev = current;
-current = next;
-}
-*head = prev;
+	while (current)
+        { 
+                next = current->next;
+                current->next = prev;
+                prev = current;
+                current = next;
+        }
+
+	*head = prev;
 }
 
 /**
@@ -29,35 +30,36 @@ current = next;
 */
 int is_palindrome(listint_t **head)
 {
-listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
-if (*head == NULL || (*head)->next == NULL)
-return (1);
-while (1)
-{
-fast = fast->next->next;
-if (!fast)
-dup = slow->next;
-break;
-}
-if (!fast->next)
-{
-dup = slow->next->next;
-break;
-}
-slow = slow->next;
-}
-reverse_listint(&dup);
-while (dup && temp)
-{
-if (temp->n == dup->n)
-{
-dup = dup->next;
-temp = temp->next;
-}
-else
-return (0);
-}
-if (!dup)
-return (1);
-return (0);
+        listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+        if (*head == NULL || (*head)->next == NULL)
+                return (1);
+        while (1)
+        {  
+                fast = fast->next->next;
+                if (!fast)
+                {
+                        dup = slow->next;
+                        break;
+                }    
+                if (!fast->next)
+                {
+                        dup = slow->next->next;
+                        break;
+                }
+                slow = slow->next;
+        }
+        reverse_listint(&dup);
+        while (dup && temp)
+        {
+                if (temp->n == dup->n)
+                {  
+                        dup = dup->next;
+                        temp = temp->next;
+                }
+                else
+                        return (0);
+        }
+        if (!dup)
+                return (1);
+        return (0);
 }
