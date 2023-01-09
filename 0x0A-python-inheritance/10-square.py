677ea2583986c82cd466829af6b2358d41079120
John Alawode <110995795+ostendoboss@users.noreply.github.com>
2023-01-09 21:53:49 +0100

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a Rectangle subclass Square."""
-Rectangle = __import__('9-rectangle').Rectangle
+"""Defines a class MyInt that inherits from int."""
 
 
-class Square(Rectangle):
-    """Represent a square"""
+class MyInt(int):
+    """Invert int operators == and !=."""
 
-    def __init__(self, size):
-        """Initialize a new square
-        """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
-        self.__size = 
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
