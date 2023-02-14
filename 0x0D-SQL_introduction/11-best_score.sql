57b12129262480af09764f6ac938f9a68f9fe428
John Alawode <110995795+ostendoboss@users.noreply.github.com>
2023-02-14 23:36:19 +0100

    updated tasks

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -3,5 +3,4 @@
 -- results display score and name
 -- ordered by score
 -- with score >= 10
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;:wq
-
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
